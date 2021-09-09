99c95a35f675d6a69fc8ddb98d009c77bcfd80ae
hayato <sakigi94@gmail.com>
2021-09-09 03:05:01 +0000

    修正最後

---
+++ app/controllers/agendas_controller.rb
@@ -0,0 +1,43 @@
+class AgendasController < ApplicationController
+  before_action :set_agenda, only: %i[  destroy]
+
+  def index
+    @agendas = Agenda.all
+  end
+
+  def new
+    @team = Team.friendly.find(params[:team_id])
+    @agenda = Agenda.new
+  end
+
+  def create
+    @agenda = current_user.agendas.build(title: params[:title])
+    @agenda.team = Team.friendly.find(params[:team_id])
+    current_user.keep_team_id = @agenda.team.id
+    if current_user.save && @agenda.save
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @team = @agenda.team
+    @users = @team.members
+    if current_user.id == @agenda.user_id || current_user.id == @team.owner.id
+      @agenda.destroy
+      AssignMailer.delete_agenda_mail(@users).deliver
+      redirect_to dashboard_path
+    else
+      redirect_to dashboard_path
+    end
+  end
+  private
+  def set_agenda
+        @agenda = Agenda.find(params[:id])
+  end
+
+  def agenda_params
+    params.fetch(:agenda, {}).permit %i[title description agenda_id]
+  end
+end
---
+++ app/mailers/assign_mailer.rb
@@ -0,0 +1,16 @@
+class AssignMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def assign_mail(email, password)
+    @email = email
+    @password = password
+    mail to: @email, subject: I18n.t('views.messages.complete_registration')
+  end
+  def delete_agenda_mail(users)
+      @email = users.pluck(:email)
+      mail to: @email, subject: I18n.t('views.messages.delete')
+  end
+  def owner_change_mail(email)
+    mail to: email, subject: I18n.t('views.messages.change_owner')
+  end
+end
